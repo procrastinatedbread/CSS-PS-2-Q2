ac570cc98c5362a28f7e61abc893b824d36ed1ee
Nikhil Chhabra <91406971+procrastinatedbread@users.noreply.github.com>
2023-06-23 02:51:17 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<div class="card">
+<img src="https://t3.ftcdn.net/jpg/01/75/78/92/240_F_175789215_rb35gQH6d4weR3d6UvOPyGvggklCtIAa.jpg"/>
+<h1>Card Title</h1>
+<p>Card description</p>
+
+</div>
+`;
